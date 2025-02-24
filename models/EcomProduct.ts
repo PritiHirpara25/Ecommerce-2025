4cc74fb0dca196aee1d17592494d841415a07ea7
PritiHirpara25 <pritihirpara256@gmail.com>
2025-02-24 10:37:45 +0530

    ...

--- models/EcomProduct.ts
+++ models/EcomProduct.ts
@@ -1,6 +1,6 @@
 export interface EcomProduct{
     Sub_Category_id : string , 
-    Product_name:string;
+    Product_name:string,
     Product_description:string,
     Product_image:string,
     Product_images:string[],
